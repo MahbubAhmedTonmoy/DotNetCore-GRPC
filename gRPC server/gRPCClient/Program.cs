d17341f2f215c936d0ae33c8e6504351f7b853aa
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-11-29 10:56:52 +0600

    product Grpc

--- gRPC server/gRPCClient/Program.cs
+++ gRPC server/gRPCClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Net.Client;
 using gRPCServer;
+using gRPCServer.Protos;
 using System;
 using System.Threading.Tasks;
 
@@ -9,12 +10,18 @@ namespace gRPCClient
     {
         static async Task Main(string[] args)
         {
-            var data = new HelloRequest { Name = "tonmoy" };
+            //var data = new HelloRequest { Name = "tonmoy" };
+            //var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(grpcChannel);
+            //var response = await client.SayHelloAsync(data);
+            //Console.WriteLine(response.Message);
+            //Console.ReadLine();
+
+            var data = new GetProductByIdModel { ProductId = 2 };
             var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(grpcChannel);
-            var response = await client.SayHelloAsync(data);
-            Console.WriteLine(response.Message);
-            Console.ReadLine();
+            var client = new Product.ProductClient(grpcChannel);
+            var response = await client.GetProductByIdAsync(data);
+            Console.WriteLine(response);
         }
     }
 }
--- gRPC server/gRPCServer/Startup.cs
+++ gRPC server/gRPCServer/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gRPCServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,7 +32,7 @@ namespace gRPCServer
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<ProductService>();
 
                 endpoints.MapGet("/", async context =>
                 {
